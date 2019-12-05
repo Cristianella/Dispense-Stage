f6da1d3d2aac8b953a2b567e4d237c6ea3d1b5b4
Cristina Mangione <cristinamangione94@gmail.com>
2019-12-05 15:55:49 +0100

    more stuff

---
+++ sorgente/Hello.java
@@ -0,0 +1,9 @@
+// Hello.java
+
+public class Hello { // Questa è la classe, il nome della classe deve essere uguale al nome del file. Il nome della classe la prima lettera deve essere Grande.
+  public static void main(String[] args) { //Questa è la mia funzione all'interno della quale ci sarà il codice. Questa è la funzione "Main". Tra parentesi ci sono i parametri
+                                           // della funzione.. Void significa che si danno i paramentri ma non si aspetta niente in cambio.
+    System.out.println("Hello!"); //ciò che la funzione "Main" fa, il comando che si fa alla CPU. Si chiama METODO: funzione all'interno della classe. ("Metodo Main").
+	                              //"Hello" è la Stringa. "Args" è un'array di Stringhe. La stampa equivale all'output.
+   }
+}
